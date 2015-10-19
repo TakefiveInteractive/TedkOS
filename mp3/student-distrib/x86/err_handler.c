c7ed89a6097b9ba76a20845f2634212ae6343454
Yifei Teng <yifei@skycatch.com>
2015-10-19 13:54:06 -0500

    Error code stack offset

--- mp3/student-distrib/x86/err_handler.c
+++ mp3/student-distrib/x86/err_handler.c
@@ -1,5 +1,8 @@
 #include <stddef.h>
+#include <stdint.h>
 #include <inc/x86/err_handler.h>
+
+const static size_t err_code_offset = (8 + 3) * sizeof(uint32_t);
 
 void exception_handler(size_t vec)
 {
