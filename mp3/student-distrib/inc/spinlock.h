d5ca010ce9c63facee9fa46f1a5c9b330c736fc3
Yifei Teng <yifei@skycatch.com>
2015-10-20 16:16:37 -0500

    Collate everyone's code

--- mp3/student-distrib/inc/spinlock.h
+++ mp3/student-distrib/inc/spinlock.h
@@ -1,18 +1,16 @@
 #ifndef _SPINLOCK_H_
 #define _SPINLOCK_H_
 
-#include <stddef.h>
-
 #define SPINLOCK_LOCKED 1
 #define SPINLOCK_UNLOCKED 0
 
-typedef int spinlock_t;
+typedef uint32_t spinlock_t;
 
 extern void spin_lock_init(spinlock_t* lock);
-extern void spin_lock_lock_irqsave(spinlock_t* lock, unsigned long flags);
-extern void spin_lock_unlock_irqrestore(spinlock_t* lock, unsigned long flags);
-extern int spin_trylock(spinlock_t* lock);
-extern int spin_is_locked(spinlock_t* lock);
-extern void spin_lock_unlock_wait(spinlock_t* lock);
+extern void spin_lock_irqsave(spinlock_t* lock, uint32_t flag);
+extern void spin_unlock_irqrestore(spinlock_t* lock, uint32_t flag);
+extern uint32_t spin_trylock(spinlock_t* lock);
+extern uint32_t spin_is_locked(spinlock_t* lock);
+extern void spin_unlock_wait(spinlock_t* lock);
 
 #endif // _SPINLOCK_H_
--- mp3/student-distrib/klibs/spinlock.c
+++ mp3/student-distrib/klibs/spinlock.c
@@ -1,32 +1,36 @@
 #include <stdint.h>
 #include <inc/spinlock.h>
 
-void spin_lock_init(spinlock_t* lock){
+void spin_lock_init(spinlock_t* lock)
+{
 
 
 }
 
-void spin_lock_lock_irqsave(spinlock_t* lock, unsigned long flags){
+void spin_lock_irqsave(spinlock_t* lock, uint32_t flags)
+{
 
 
 }
 
-void spin_lock_unlock_irqrestore(spinlock_t* lock, unsigned long flags){
+void spin_unlock_irqrestore(spinlock_t* lock, uint32_t flags)
+{
 
 
 }
 
-int spin_trylock(spinlock_t* lock){
-
-
+uint32_t spin_trylock(spinlock_t* lock)
+{
+    return 0;
 }
 
-int spin_is_locked(spinlock_t* lock){
-
-
+uint32_t spin_is_locked(spinlock_t* lock)
+{
+    return 1;
 }
 
-void spin_lock_unlock_wait(spinlock_t* lock){
+void spin_lock_unlock_wait(spinlock_t* lock)
+{
 
 
 }
