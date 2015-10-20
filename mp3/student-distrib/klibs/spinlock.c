0350e297239e888198058d39ae590a59c9b5e2dd
Han <yanhan02008@gmail.com>
2015-10-20 15:44:45 -0500

    add kb int handler

--- mp3/student-distrib/klibs/spinlock.c
+++ mp3/student-distrib/klibs/spinlock.c
@@ -1,5 +1,5 @@
-//#include <inc/x86/spinlock.h>
-#include "spinlock.h"
+#include <inc/x86/spinlock.h>
+//#include "spinlock.h"
 
 void spin_lock_init(spinlock_t* lock){
 
@@ -28,5 +28,5 @@ int spin_is_locked(spinlock_t* lock){
 
 void spin_lock_unlock_wait(spinlock_t* lock){
 
-    
+
 }
