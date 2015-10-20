6fc6b90f264f2c73393694346a955b49e7a35ce3
Yifei Teng <yifei@skycatch.com>
2015-10-20 15:26:49 -0500

    Correct storage modifier

--- mp3/student-distrib/inc/x86/idt_table.h
+++ mp3/student-distrib/inc/x86/idt_table.h
@@ -5,7 +5,7 @@ typedef void (*vector_extracting_handler)(void) __attribute__((fastcall));
 #ifdef __cplusplus
 extern "C" {
 #endif
-    extern static const unsigned long int ErrorCodeInExceptionBitField;
+    extern const unsigned long int ErrorCodeInExceptionBitField;
     extern vector_extracting_handler interrupt_table[255];
 #ifdef __cplusplus
 }
