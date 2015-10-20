abf443a5e1b652389cb546181b02e366d27be3cc
Yifei Teng <yifei@skycatch.com>
2015-10-20 14:05:06 -0500

    Primitive implementation of exception

--- mp3/student-distrib/inc/x86/err_handler.h
+++ mp3/student-distrib/inc/x86/err_handler.h
@@ -4,7 +4,16 @@
 /*
  * This function is invoked by the common interrupt function when an 'error' occurs.
  */
-void exception_handler(size_t);
+void __attribute__((fastcall)) exception_handler(size_t, unsigned long int);
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+    extern const unsigned long int ErrorCodeInExceptionBitField;
+#ifdef __cplusplus
+}
+#endif
+
 
 #endif
 
