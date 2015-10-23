56c4f8ee81a1ef78282889676b245c9f3dea53fd
Han <yanhan02008@gmail.com>
2015-10-23 17:04:06 -0500

    add 1.1

--- mp3/student-distrib/inc/spinlock.h
+++ mp3/student-distrib/inc/spinlock.h
@@ -4,11 +4,19 @@
 #define SPINLOCK_LOCKED 1
 #define SPINLOCK_UNLOCKED 0
 
+#include <stdint.h>
+#include <stddef.h>
+
 typedef uint32_t spinlock_t;
+
+extern void spin_unlock(spinlock_t* lock);
+extern void spin_lock(spinlock_t* lock);
 
 extern void spin_lock_init(spinlock_t* lock);
 extern void spin_lock_irqsave(spinlock_t* lock, uint32_t flag);
 extern void spin_unlock_irqrestore(spinlock_t* lock, uint32_t flag);
+
+
 extern uint32_t spin_trylock(spinlock_t* lock);
 extern uint32_t spin_is_locked(spinlock_t* lock);
 extern void spin_unlock_wait(spinlock_t* lock);
--- mp3/student-distrib/klibs/spinlock.c
+++ mp3/student-distrib/klibs/spinlock.c
@@ -7,15 +7,23 @@ void spin_lock_init(spinlock_t* lock)
 
 }
 
-void spin_lock_irqsave(spinlock_t* lock, uint32_t flags)
+void spin_lock(spinlock_t* lock)
 {
+    *lock = SPINLOCK_LOCKED;
+}
 
+void spin_unlock(spinlock_t* lock)
+{
+    *lock = SPINLOCK_UNLOCKED;
+}
+
+void spin_lock_irqsave(spinlock_t* lock, uint32_t flag)
+{
 
 }
 
-void spin_unlock_irqrestore(spinlock_t* lock, uint32_t flags)
+void spin_unlock_irqrestore(spinlock_t* lock, uint32_t flag)
 {
-
 
 }
 
