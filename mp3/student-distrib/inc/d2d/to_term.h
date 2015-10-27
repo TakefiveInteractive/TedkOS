acc79efad18ecccc06a866f4d0ed615e80767a0c
C4Phone <qq2900726136@gmail.com>
2015-10-27 12:12:53 -0500

    clearer explannation about d2d/to_term

--- mp3/student-distrib/inc/d2d/to_term.h
+++ mp3/student-distrib/inc/d2d/to_term.h
@@ -6,13 +6,18 @@
 void kb_to_term(int32_t kenerlKeycode);
 
 // Definitions of kernel level keycodes are defined here as well.
-// 1. For keys that have an ASCII character, their kernel keycode is equal to ASCII.
+// 1. For keys that have a PRINTABLE character, their kernel keycode is equal to ASCII.
 // 2. Otherwise for keys that combine with other keys, their keycode starts
-//		from 0x10000
+//        from 0x10000
 // 3. Otherwise the keys are represented using values from 0x100 to 0xff00
-#define CTRL	0x10000
-#define ALT		0x20000
-#define SHIFT	0x40000
+
+// define a = 'a'. define A = 'a' | KKC_SHIFT
+
+// KKC is short for kernel keycode
+
+#define KKC_CTRL    0x10000
+#define KKC_ALT     0x20000
+#define KKC_SHIFT   0x40000
 
 // and so on
 
