f5d18647ad60baef539dc165dba88345310557fc
Yu Wang <wangyu2165@gmail.com>
2015-10-27 17:05:47 -0500

    rtc advanced done

--- mp3/student-distrib/drivers/rtc.c
+++ mp3/student-distrib/drivers/rtc.c
@@ -1,5 +1,9 @@
 #include <inc/rtc.h>
 
+uint8_t frequency_converter(int freq);
+void rtc_change_frequency(uint8_t rate);
+int interrupt_flag = 0;
+
 DEFINE_DRIVER_INIT(rtc)
 {
     bind_irq(RTC_IRQ, RTC_ID, rtc_handler, RTC_POLICY);
@@ -13,6 +17,9 @@ DEFINE_DRIVER_REMOVE(rtc)
     return;
 }
 
+/**
+ * rtc_init() is used to initialize rtc
+ */
 void rtc_init()
 {
     cli();
@@ -20,15 +27,6 @@ void rtc_init()
     uint8_t prev = inb(RTC_DATA);
     outb(RTC_STATUS_B_NMI, RTC_ADDRESS);
     outb(prev | RTC_STATUS_B_EN, RTC_DATA);
-    sti();
-
-    // rate must be above 2 and not over 15
-    uint8_t rate = 0x0F;
-    cli();
-    outb(RTC_STATUS_A_NMI, RTC_ADDRESS);
-    prev = inb(RTC_DATA);
-    outb(RTC_STATUS_A_NMI, RTC_ADDRESS);
-    outb((prev & HIGH_BIT_MASK) | rate, RTC_DATA);
     sti();
 }
 
@@ -42,8 +40,106 @@ void rtc_init()
  */
 int rtc_handler(int irq, unsigned int saved_reg)
 {
-    test_interrupts();
+    cli();
+    /* test_interrupts(); */
+    /* read register c to allow future use */
     outb(RTC_STATUS_C, RTC_ADDRESS);
     inb(RTC_DATA);
+    interrupt_flag = 1;
+    sti();
     return 0;
+}
+
+/**
+ * read() would block user program until rtc interrupt finished
+ * @param  fd     [description]
+ * @param  buf    [description]
+ * @param  nbytes [description]
+ * @return        [description]
+ */
+int read (int fd, void* buf, int nbytes)
+{
+    while (interrupt_flag == 0) {;}
+    interrupt_flag = 0;
+    return 0;
+}
+
+/**
+ * write() would change the frequency of the rtc interrupt generate
+ * @param  fd     [description]
+ * @param  buf    buf[0] has the frequency number
+ * @param  nbytes [description]
+ * @return        0 for success, -1 for frequency out of range
+ */
+int write (int fd, const void* buf, int nbytes)
+{
+    int freq = *(int *)buf;
+    int rate = frequency_converter(freq);
+    if (!rate) return -1;
+    rtc_change_frequency(rate);
+    return 0;
+}
+
+/**
+ * open() would initialize the rtc rate to 2Hz
+ * @param  filename [description]
+ * @return          return 0 always
+ */
+int open (const uint8_t* filename)
+{
+    /* open would initialize rtc frequency to 2Hz */
+    uint8_t rate = frequency_converter(2);
+    rtc_change_frequency(rate);
+    return 0;
+}
+
+/**
+ * close() did nothing, as TA mentioned
+ * @param  fd [description]
+ * @return    always return 0
+ */
+int close (int fd)
+{
+    return 0;
+}
+
+/**
+ * private function that convert user input frequency to rtc binary
+ * @param  freq - frequency of the rtc, e.g. 2 for 2Hz
+ * @return      rtc understandable binary presentation of actual frequency
+ */
+uint8_t frequency_converter(int freq)
+{
+    // rate must be above 0x02 and not over 0x0F
+    switch (freq) {
+        case 16384: return 0x02;
+        case 8192: return 0x03;
+        case 4096: return 0x04;
+        case 2048: return 0x05;
+        case 1024: return 0x06;
+        case 512: return 0x07;
+        case 256: return 0x08;
+        case 128: return 0x09;
+        case 64: return 0x0A;
+        case 32: return 0x0B;
+        case 16: return 0x0C;
+        case 8: return 0x0D;
+        case 4: return 0x0E;
+        case 2: return 0x0F;
+        default: return 0;
+    }
+}
+
+/**
+ * private helper function for write() that changes the rate of rtc
+ * @param rate - rtc understandable binary, range from 0x02 to 0x0F
+ */
+void rtc_change_frequency(uint8_t rate)
+{
+    cli();
+    outb(RTC_STATUS_A_NMI, RTC_ADDRESS);
+    uint8_t prev = inb(RTC_DATA);
+    outb(RTC_STATUS_A_NMI, RTC_ADDRESS);
+    outb((prev & HIGH_BIT_MASK) | rate, RTC_DATA);
+    sti();
 }
