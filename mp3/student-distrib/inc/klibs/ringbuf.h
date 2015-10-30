9a3701ece2d012afb0ad63d484c125f53f14edb6
C4Phone <qq2900726136@gmail.com>
2015-10-29 21:54:58 -0500

    Try backspace

---
+++ mp3/student-distrib/inc/klibs/ringbuf.h
@@ -0,0 +1,81 @@
+#ifndef _KLIBS_RINGBUF_H
+#define _KLIBS_RINGBUF_H
+
+#ifndef RINGBUF_SIZE
+#warning "RINGBUF_SIZE is not defined, using 128"
+#define RINGBUF_SIZE        128
+#else
+#if(RINGBUF_SIZE <= 1)
+#error "RINGBUF_SIZE should be at least 2."
+#endif
+#endif
+
+#ifndef RINGBUF_TYPE
+#error "RINGBUF_TYPE is not defined"
+#endif
+
+#include <stdint.h>
+
+// This file should be used like template.
+// (But in every .c the RINGBUF_SIZE can have only one value, though...
+
+// ! Important !
+// This data structure is NOT protected by lock!
+// Because maybe different usage require different arrangement of locks.
+
+// Similar to queues, but different:
+//      if queue is full, nothing is pushed in
+//      if ringbuf is full, it kicks out the oldest element
+typedef struct 
+{
+    RINGBUF_TYPE data[RINGBUF_SIZE];
+
+    // stores the actual RINGBUF_SIZE 
+    //  this value varies by .c file,
+    //  thus the implementation must know it.
+
+    int32_t _macro_ringbuf_size;
+
+    // stores the size of the type used
+    int32_t _macro_ringbuf_type_size;
+
+    // position of next poped value.
+    // -1 if empty
+    int32_t head;
+
+    // position of next pushed value.
+    int32_t tail;
+
+    // actual number of items stored.
+    int32_t len;
+} ringbuf_t;
+
+
+#define RINGBUF_INIT(BUF)                                   \
+{                                                           \
+    (BUF)->head = -1;                                       \
+    (BUF)->tail = 0;                                        \
+    (BUF)->len = 0;                                         \
+    (BUF)->_macro_ringbuf_size = RINGBUF_SIZE;              \
+    (BUF)->_macro_ringbuf_type_size = sizeof(RINGBUF_TYPE); \
+}
+
+int32_t ringbuf_length(ringbuf_t* buf);
+
+int ringbuf_is_empty(ringbuf_t* buf);
+
+void ringbuf_push(ringbuf_t* buf, void* item);
+
+// Return the item that is nearest to be poped
+// But _front does not actually pop it.
+//  If empty, returns non-zero. success <=> 0
+int ringbuf_front(ringbuf_t* buf, void* result);
+
+// This function does not do memcpy, so be careful
+//      not to access the pointer after pop.
+// If empty, returns null.
+void* ringbuf_front_nocp(ringbuf_t* buf);
+
+void ringbuf_pop(ringbuf_t* buf);
+
+#endif /* _KLIBS_RINGBUF_H */
---
+++ mp3/student-distrib/klibs/ringbuf.c
@@ -0,0 +1,74 @@
+#define RINGBUF_SIZE    2
+#define RINGBUF_TYPE    uint8_t
+// We use uint8_t as type because we want
+//      unit to be 1 byte.
+// We are the implementation and we do not set
+//      the value of RINGBUF_SIZE.
+//  we only need to read that value,
+//      by reading ringbuf._macro_ringbuf_size
+
+#include <inc/klibs/ringbuf.h>
+#include <inc/klibs/lib.h>
+#include <stddef.h>
+#include <stdint.h>
+
+int32_t ringbuf_length(ringbuf_t* buf)
+{
+    return buf->len;
+}
+
+int ringbuf_is_empty(ringbuf_t* buf)
+{
+    return buf->head == -1;
+}
+
+void ringbuf_push(ringbuf_t* buf, void* item)
+{
+    if(buf->head == -1)
+        buf->head = buf->tail;
+    else if(buf->tail == buf->head)
+    {
+        // the 'else' is very important.
+        // In this case we clobber old value.
+        buf->head = (buf->head + 1) % buf->_macro_ringbuf_size;
+        buf->len--;
+    }
+    memcpy(buf->data + buf->tail * buf->_macro_ringbuf_type_size, item, buf->_macro_ringbuf_type_size);
+    buf->tail = (buf->tail + 1) % buf->_macro_ringbuf_size;
+    buf->len++;
+}
+
+// Return the item that is nearest to be poped
+// But _front does not actually pop it.
+//  If empty, returns non-zero. success <=> 0
+int ringbuf_front(ringbuf_t* buf, void* result)
+{
+    if(buf->head == -1)
+        return -1;
+    else
+    {
+        memcpy(result, buf->data + buf->head * buf->_macro_ringbuf_type_size, buf->_macro_ringbuf_type_size);
+        return 0;
+    }
+}
+
+// This function does not do memcpy, so be careful
+//      not to access the pointer after pop.
+// If empty, returns null.
+void* ringbuf_front_nocp(ringbuf_t* buf)
+{
+    if(buf->head == -1)
+        return NULL;
+    else
+        return buf->data + buf->head * buf->_macro_ringbuf_type_size;
+}
+
+void ringbuf_pop(ringbuf_t* buf)
+{
+    if(buf->head == -1)
+        return;
+    buf->head = (buf->head + 1) % buf->_macro_ringbuf_size;
+    buf->len--;
+    if(buf->len==0)
+        buf->head = -1;
+}
