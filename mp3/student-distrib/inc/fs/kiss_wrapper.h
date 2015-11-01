e4c54f58f8a2eb694af690c5f23c2044828d3ae4
Yifei Teng <yifei@skycatch.com>
2015-10-31 23:35:57 -0500

    Move mbi address to global

--- mp3/student-distrib/inc/fs/kiss_wrapper.h
+++ mp3/student-distrib/inc/fs/kiss_wrapper.h
@@ -7,12 +7,12 @@
 #include <inc/multiboot.h>
 #include <inc/lphashtable.h>
 #include <inc/klibs/lib.h>
-#include <inc/fs/filesystem.h>
+#include <inc/fs/kiss.h>
 
 
 #ifdef __cplusplus
 namespace filesystem {
-    void init_from_multiboot(multiboot_info *mbi);
+    void init_from_multiboot();
     int32_t read_dentry(const uint8_t* fname, dentry_t* dentry);
     int32_t read_dentry(uint32_t index, dentry_t* dentry);
     int32_t read_data(uint32_t inode, uint32_t offset, uint8_t* buf, uint32_t length);
