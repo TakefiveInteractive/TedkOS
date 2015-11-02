b12a9cbaa011a3d443ec682650e864b6eeebf726
C4Phone <qq2900726136@gmail.com>
2015-11-02 16:54:30 -0600

    final user improvements

--- mp3/student-distrib/tests/kreader.cpp
+++ mp3/student-distrib/tests/kreader.cpp
@@ -12,6 +12,79 @@
 
 #define termputarr(FD, ARR) {fs_write((FD), (const void*)(ARR), sizeof(ARR)/sizeof(char));}
 
+
+/*
+* int8_t* user_strrev(int8_t* s);
+*   Inputs: int8_t* s = string to reverse
+*   Return Value: reversed string
+*	Function: reverses a string s
+*/
+
+char*
+user_strrev(char* s)
+{
+	register char tmp;
+	register int32_t beg=0;
+	register int32_t end=strlen(s) - 1;
+
+	while(beg < end) {
+		tmp = s[end];
+		s[end] = s[beg];
+		s[beg] = tmp;
+		beg++;
+		end--;
+	}
+
+	return s;
+}
+
+
+/*
+* reference: klibs/lib.c
+* user side itoa
+* int8_t* user_itoa(uint32_t value, int8_t* buf, int32_t radix);
+*   Inputs: uint32_t value = number to convert
+*			int8_t* buf = allocated buffer to place string in
+*			int32_t radix = base system. hex, oct, dec, etc.
+*   Return Value: number of bytes written
+*	Function: Convert a number to its ASCII representation, with base "radix"
+*/
+
+char*
+user_itoa(uint32_t value, char* buf, int32_t radix)
+{
+	static char lookup[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	char *newbuf = buf;
+	int32_t i;
+	uint32_t newval = value;
+
+	/* Special case for zero */
+	if(value == 0) {
+		buf[0]='0';
+		buf[1]='\0';
+		return buf;
+	}
+
+	/* Go through the number one place value at a time, and add the
+	 * correct digit to "newbuf".  We actually add characters to the
+	 * ASCII string from lowest place value to highest, which is the
+	 * opposite of how the number should be printed.  We'll reverse the
+	 * characters later. */
+	while(newval > 0) {
+		i = newval % radix;
+		*newbuf = lookup[i];
+		newbuf++;
+		newval /= radix;
+	}
+
+	/* Add a terminating NULL */
+	*newbuf = '\0';
+
+	/* Reverse the string and return */
+	return user_strrev(buf);
+}
+
 // Currently new "instance" only needs stdin and stdout
 int kreader_main (int32_t term, int32_t kb)
 {
@@ -35,27 +108,6 @@ int kreader_main (int32_t term, int32_t kb)
         if ('\0' == buf[0])
             continue;
 
-        if (cnt == 2 && buf[0] == '/' && buf[1] == '.')
-        {
-            int32_t fd, cnt;
-
-            if (-1 == (fd = fs_open("/."))) {
-                termputarr(term, "directory open failed\n");
-                continue;
-            }
-
-            while (0 != (cnt = fs_read(fd, filebuf, sizeof(filebuf) - 1))) {
-                if (-1 == cnt) {
-                    termputarr(term, "directory entry read failed\n");
-                    continue;
-                }
-                filebuf[cnt] = '\n';
-                filebuf[cnt + 1] = '\0';
-                printf("%s", filebuf);
-            }
-
-            continue;
-        }
         int fd = fs_open((const char *)buf);
         if (-1 == fd)
         {
@@ -63,6 +115,10 @@ int kreader_main (int32_t term, int32_t kb)
         }
         else
         {
+            // 20 chars are enough for 2^64
+            #define lenOfStrOfInt 21
+            char strOfInt[lenOfStrOfInt];
+            int totalLen = 0;
             int len = 0;
             do
             {
@@ -71,9 +127,14 @@ int kreader_main (int32_t term, int32_t kb)
                 if (len > 0)
                 {
                     termputarr(term, "\n\npress enter to read next block...\n");
+                    totalLen += len;
                     fs_read (kb, (void*)buf, 1);
                 }
             } while (len > 0);
+            termputarr(term, "\n\n Total size: ");
+            user_itoa(totalLen, strOfInt, 10);
+            fs_write(term, strOfInt, lenOfStrOfInt);
+            termputarr(term, "\n\n");
         }
     }
     return 0;
