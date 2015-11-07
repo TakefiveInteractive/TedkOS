a07a09ff0b23df5b10de903a60545a65f4e4c20a
Yu Wang <wangyu2165@gmail.com>
2015-11-06 22:36:27 -0600

    modiy exec header

--- mp3/student-distrib/syscalls/exec.h
+++ mp3/student-distrib/syscalls/exec.h
@@ -17,7 +17,7 @@ int8_t is_kiss_executable(const uint8_t* file);
 //   Load "file", an executable, to "program_image_addr"
 //   You can assume that CR3 already points the page table of this process.
 // Return 0 if success. Return -ENOMEM if file is too large (not possible, though)
-int32_t kiss_loader(const uint8_t* file);
+void* kiss_loader(const uint8_t* file);
 
 // Main entry to implementation of exec syscall
 int32_t do_exec(const uint8_t* file);
