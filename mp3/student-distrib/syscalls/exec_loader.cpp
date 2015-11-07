74063bd51d63b6bc6a87fef678c6ba2a7a91dee4
Yu Wang <wangyu2165@gmail.com>
2015-11-06 22:35:19 -0600

    now kiss_loader return the main(entry point) addr

--- mp3/student-distrib/syscalls/exec_loader.cpp
+++ mp3/student-distrib/syscalls/exec_loader.cpp
@@ -7,19 +7,19 @@ using namespace syscall_exec;
 //   Load "file", an executable, to "program_image_addr"
 //   You can assume that CR3 already points the page table of this process.
 // Return 0 if success. Return -ENOMEM if file is too large (not possible, though)
-int32_t kiss_loader(const uint8_t* file)
+void* kiss_loader(const uint8_t* file)
 {
     dentry_t dentry;
     int32_t res = read_dentry_by_name(file, &dentry);
-    if (res == -1) return -EINVAL;
+    if (res == -1) return NULL;
 
     const uint32_t four_mb = 0x400000;
     uint8_t* curr = (uint8_t*)program_image_addr;
     uint8_t test_over_flow;
     uint32_t first_len = read_data(dentry.inode, 0, curr, four_mb);
-    if(first_len == four_mb)
-        if(read_data(dentry.inode, four_mb, &test_over_flow, 1) > 0)
-            return -ENOMEM;
+    if (first_len == four_mb)
+        if (read_data(dentry.inode, four_mb, &test_over_flow, 1) > 0)
+            return NULL;
 
-    return 0;
+    return *(uint32_t**)(program_image_addr + 24);
 }
