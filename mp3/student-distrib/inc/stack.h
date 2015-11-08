d4ae679a249a73c07ed1849fae81dc132ab42ddd
Yifei Teng <yifei@skycatch.com>
2015-11-07 21:54:03 -0600

    Fix all errors and finish allocator

--- mp3/student-distrib/inc/stack.h
+++ mp3/student-distrib/inc/stack.h
@@ -1,6 +1,8 @@
 #ifndef _INC_STACK_H_
 #define _INC_STACK_H_
 
+#include <inc/maybe.h>
+
 namespace util {
 
 template<typename T, size_t Num>
@@ -46,6 +48,26 @@ class Stack {
                 arr[idx] = val;
                 idx++;
             }
+        }
+
+        Maybe<void *> first(Maybe<void *> (*fn) (T))
+        {
+            for (size_t i = 0; i < idx; i++)
+            {
+                auto x = fn(arr[idx]);
+                if (x) return x;
+            }
+            return Maybe<void *>();
+        }
+
+        bool firstTrue(void *a, bool (*fn) (T, void*))
+        {
+            for (size_t i = 0; i < idx; i++)
+            {
+                auto x = fn(arr[idx], a);
+                if (x) return x;
+            }
+            return false;
         }
 };
 
