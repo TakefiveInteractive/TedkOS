b6235413cb61e8d42e08cd1b976f3cc63f20ac5a
Yifei Teng <yifei@skycatch.com>
2015-11-12 01:08:12 -0600

    More clean-up. Make sure cp2 stuff are working

--- mp3/student-distrib/fs/kissfs_wrapper.cpp
+++ mp3/student-distrib/fs/kissfs_wrapper.cpp
@@ -24,20 +24,3 @@ int32_t read_data(const uint32_t inode, uint32_t offset, uint8_t *buf, uint32_t 
 
 }
 
-/* C interface */
-
-int32_t read_dentry_by_name (const uint8_t* fname, dentry_t* dentry)
-{
-    return filesystem::read_dentry(fname, dentry);
-}
-
-int32_t read_dentry_by_index (uint32_t index, dentry_t* dentry)
-{
-    return filesystem::read_dentry(index, dentry);
-}
-
-int32_t read_data (uint32_t inode, uint32_t offset, uint8_t* buf, uint32_t length)
-{
-    return filesystem::read_data(inode, offset, buf, length);
-}
-
