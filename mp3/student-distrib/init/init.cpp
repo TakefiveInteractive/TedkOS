a7b3e4e1c21d9598819327bd37ef4b2c3c2f5e41
C4Phone <qq2900726136@gmail.com>
2015-11-11 23:22:50 -0600

    fixed bugs. First KERNEL thread: init

--- mp3/student-distrib/init/init.cpp
+++ mp3/student-distrib/init/init.cpp
@@ -1,7 +1,9 @@
 #include <inc/init.h>
+#include <inc/klibs/lib.h>
 
 void init_main()
 {
+    printf("Hello!\nI am the idle process!\nI am a kernel process!\nI am every other process's parent!\n");
     /* Spin (nicely, so we don't chew up cycles) */
     asm volatile("1: hlt; jmp 1b;");
 }
