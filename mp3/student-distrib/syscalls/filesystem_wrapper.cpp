a55d9b239aa3bde79252959085fb2c540b203cf9
Yu Wang <wangyu2165@gmail.com>
2015-11-12 23:32:59 -0600

    Integrate filesystem into syscall

---
+++ mp3/student-distrib/syscalls/filesystem_wrapper.cpp
@@ -0,0 +1,57 @@
+#include <inc/syscalls/filesystem_wrapper.h>
+#include <inc/fs/filesystem.h>
+#include <inc/proc/tasks.h>
+
+using namespace filesystem;
+using namespace palloc;
+
+bool check_valid_fd(int32_t fd, ProcessDesc *processDesc)
+{
+    if (fd >= FD_ARRAY_LENGTH || fd < 0)
+        return false;
+    if (!processDesc->fileDescs[fd]) return false;
+    return true;
+}
+
+int32_t fs_read(int32_t fd, void *buf, int32_t nbytes)
+{
+    auto processDesc = getCurrentThreadInfo()->pcb.to_process;
+    if (!check_valid_fd(fd, processDesc)) return -1;
+    return theDispatcher->read(*processDesc->fileDescs[fd], buf, nbytes);
+}
+
+int32_t fs_write(int32_t fd, const void *buf, int32_t nbytes)
+{
+    auto processDesc = getCurrentThreadInfo()->pcb.to_process;
+    if (!check_valid_fd(fd, processDesc)) return -1;
+    return theDispatcher->write(*processDesc->fileDescs[fd], buf, nbytes);
+}
+
+int32_t fs_open(const char *filename)
+{
+    auto processDesc = getCurrentThreadInfo()->pcb.to_process;
+    File *fd = new File;
+    if (processDesc->numFilesInDescs >= FD_ARRAY_LENGTH)
+        return -1;
+    bool res = theDispatcher->open(*fd, filename);
+    if (!res) {
+        delete fd;
+        return -1;
+    }
+    processDesc->fileDescs[processDesc->numFilesInDescs] = fd;
+    return processDesc->numFilesInDescs++;
+}
+
+int32_t fs_close(int32_t fd)
+{
+    auto processDesc = getCurrentThreadInfo()->pcb.to_process;
+    if (!check_valid_fd(fd, processDesc)) return -1;
+    bool res = theDispatcher->close(*processDesc->fileDescs[fd]);
+    if (!res) return -1;
+    if (processDesc->fileDescs[fd])
+    {
+        delete processDesc->fileDescs[fd];
+        processDesc->fileDescs[fd] = nullptr;
+    }
+    return 0;
+}
