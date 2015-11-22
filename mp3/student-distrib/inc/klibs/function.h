d147b1108c9fb45f7d0dcbe7dde1d720b6546011
Yifei Teng <yifei@skycatch.com>
2015-11-22 15:10:31 -0600

    Enable void return values

--- mp3/student-distrib/inc/klibs/function.h
+++ mp3/student-distrib/inc/klibs/function.h
@@ -13,8 +13,8 @@ struct abstract_function
     virtual ~abstract_function() = default;
 };
 
-template<typename Func,typename Result,typename ...Args>
-class concrete_function: public abstract_function<Result,Args...>
+template<typename Func, typename Result, typename ...Args>
+class concrete_function: public abstract_function<Result, Args...>
 {
     Func f;
 public:
@@ -22,6 +22,26 @@ public:
     Result operator() (Args... args) override
     {
         return f(args...);
+    }
+
+    concrete_function *clone(MemoryPool<uint8_t[64], 2> &pool) const override
+    {
+        return new (pool) concrete_function { f };
+    }
+
+    // custom new and delete
+    void *operator new(size_t s, MemoryPool<uint8_t[64], 2>& hp)   { return reinterpret_cast<concrete_function*>(hp.get()); }
+};
+
+template<typename Func, typename ...Args>
+class concrete_function<Func, void, Args...>: public abstract_function<void, Args...>
+{
+    Func f;
+public:
+    concrete_function(const Func &x) : f(x) { }
+    void operator() (Args... args) override
+    {
+        f(args...);
     }
 
     concrete_function *clone(MemoryPool<uint8_t[64], 2> &pool) const override
@@ -47,10 +67,10 @@ struct func_filter<Result(Args...)>
 template<typename signature>
 class function;
 
-template<typename Result,typename ...Args>
+template<typename Result, typename ...Args>
 class function<Result(Args...)>
 {
-    abstract_function<Result,Args...> *f;
+    abstract_function<Result, Args...> *f;
     // Size-agnostic pool. 64 should be enough for most functions
     MemoryPool<uint8_t[64], 2> pool;
 
@@ -59,7 +79,7 @@ public:
 
     template<typename Func> function(const Func &x)
     {
-        f = new (pool) concrete_function<typename func_filter<Func>::type,Result,Args...>(x);
+        f = new (pool) concrete_function<typename func_filter<Func>::type, Result, Args...>(x);
     }
 
     function(const function &other)
@@ -77,7 +97,40 @@ public:
 
     ~function()
     {
-        f->~abstract_function<Result,Args...>();
+        f->~abstract_function<Result, Args...>();
+        pool.release(reinterpret_cast<uint8_t(*)[64]>(f));
+    }
+};
+
+template<typename ...Args>
+class function<void(Args...)>
+{
+    abstract_function<void, Args...> *f;
+    // Size-agnostic pool. 64 should be enough for most functions
+    MemoryPool<uint8_t[64], 2> pool;
+
+public:
+    function() : f(nullptr) { }
+
+    template<typename Func> function(const Func &x)
+    {
+        f = new (pool) concrete_function<typename func_filter<Func>::type, void, Args...>(x);
+    }
+
+    function(const function &other)
+    {
+        f = other.f->clone(pool);
+    }
+
+    void operator()(Args... args)
+    {
+        if (f)
+           (*f)(args...);
+    }
+
+    ~function()
+    {
+        f->~abstract_function<void, Args...>();
         pool.release(reinterpret_cast<uint8_t(*)[64]>(f));
     }
 };
