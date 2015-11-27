9e6dbaa727dad71070f5aa3945e62a41ea7ed98d
Yifei Teng <yifei@skycatch.com>
2015-11-27 01:09:46 -0600

    Hook up fstat and lseek

--- mp3/shared/ece391sysnum.h
+++ mp3/shared/ece391sysnum.h
@@ -1,16 +1,18 @@
 #if !defined(ECE391SYSNUM_H)
 #define ECE391SYSNUM_H
 
-#define SYS_HALT    1
-#define SYS_EXECUTE 2
-#define SYS_READ    3
-#define SYS_WRITE   4
-#define SYS_OPEN    5
-#define SYS_CLOSE   6
-#define SYS_GETARGS 7
-#define SYS_VIDMAP  8
-#define SYS_SET_HANDLER  9
-#define SYS_SIGRETURN  10
-#define SYS_SBRK  11
+#define SYS_HALT        1
+#define SYS_EXECUTE     2
+#define SYS_READ        3
+#define SYS_WRITE       4
+#define SYS_OPEN        5
+#define SYS_CLOSE       6
+#define SYS_GETARGS     7
+#define SYS_VIDMAP      8
+#define SYS_SET_HANDLER 9
+#define SYS_SIGRETURN   10
+#define SYS_SBRK        11
+#define SYS_FSTAT       12
+#define SYS_LSEEK       13
 
 #endif /* ECE391SYSNUM_H */
