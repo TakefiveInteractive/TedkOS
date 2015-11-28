43ae48c5afc6571e1b97e34ac692f43c6db91a0b
C4Phone <qq2900726136@gmail.com>
2015-11-28 13:44:05 -0600

    fix argument validation

--- mp3/student-distrib/syscalls/getargs.cpp
+++ mp3/student-distrib/syscalls/getargs.cpp
@@ -6,7 +6,7 @@ using namespace filesystem;
 int32_t getargs (uint8_t* buf, int32_t nbytes)
 {
     auto pd = getCurrentThreadInfo()->pcb.to_process;
-    if (!pd->arg) return -1;
+    if (!pd->arg) {buf[0] = '\0'; return 0;}
     int32_t arg_len = strlen(pd->arg);
     if (nbytes < arg_len + 1) return -1;
     memcpy(buf, pd->arg, arg_len);
--- mp3/student-distrib/syscalls/vidmap.cpp
+++ mp3/student-distrib/syscalls/vidmap.cpp
@@ -7,10 +7,15 @@
 namespace syscall
 {
 
+#define HAS_FLAG(X, FLAG) (((X) & (FLAG)) == (FLAG))
+
 int32_t vidmap (uint8_t** screen_start)
 {
+    if(!screen_start)
+        return -1;
     uint32_t pde = global_cr3val[((uint32_t)screen_start) >> 22];
-    if((pde & (PT_BASE | PT_USER)) || (pde | (PG_4MB_BASE & PG_USER)))
+    // TODO: also check PTE!!!
+    if(HAS_FLAG(pde, PT_BASE|PT_USER) || HAS_FLAG(pde, PG_4MB_BASE|PG_USER))
     {
         getCurrentThreadInfo()->pcb.to_process->memmap.enableVidmap(palloc::cpu0_memmap);
         *screen_start = (uint8_t*)get_vmem_phys();
