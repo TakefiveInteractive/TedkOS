3eecb6daf39ee2d3ab9f6f86cbccceaa1e868899
Yu Wang <wangyu2165@gmail.com>
2015-12-01 21:23:27 -0600

    font to png script working version

--- mp3/gui/font_to_png_tedkos.py
+++ mp3/gui/font_to_png_tedkos.py
@@ -7,8 +7,6 @@ args = list(sys.argv)[1:]
 def convert(arg):
     r = png.Reader(arg)
     bitmap = r.read()[2]
-    colCount = 0
-    whitespaceLine = []
     stdColLen = 0
 
     switcher = {
@@ -19,13 +17,11 @@ def convert(arg):
     stdColLen = switcher[os.path.basename(arg)[0]]
     with open(arg + '.conv', 'wb') as f:
         for row in bitmap:
-            if colCount == 0:
-                colCount = len(row)
-            whitespaceLine = row
             f.write(bytearray(row))
-        if colCount < stdColLen:
-            for i in range(stdColLen - colCount):
-                f.write(bytearray(whitespaceLine))
+            if len(row) < stdColLen:
+                for i in range((stdColLen - len(row))):
+                    f.write(bytearray([0]))
+
 
 for arg in args:
     convert(arg)
