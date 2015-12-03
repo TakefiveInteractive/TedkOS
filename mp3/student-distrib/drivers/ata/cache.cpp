4cbaeb8a28997581b7119fe48f86ea3cd35e6caa
Yifei Teng <yifei@skycatch.com>
2015-12-03 16:04:12 -0600

    Import all ATA code

---
+++ mp3/student-distrib/drivers/ata/cache.cpp
@@ -0,0 +1,72 @@
+#include "ata_priv.h"
+
+namespace ata {
+
+constexpr size_t cache_size = 4096;
+
+struct cache_element {
+    bool inuse;
+    size_t deviceid;
+    size_t sector;
+    size_t usecount;
+    char data[ATA_SECTOR_SIZE];
+};
+
+static cache_element cache[cache_size];
+
+static bool find_free(size_t &index){
+    for(size_t i=0; i<cache_size; ++i){
+        if(!cache[i].inuse){
+            index=i;
+            return true;
+        }
+    }
+    return false;
+}
+
+static void prune_cache(){
+    size_t min_uses=0xFFFFFFFF;
+    for(size_t i=0; i<cache_size; ++i){
+        if(cache[i].inuse && cache[i].usecount < min_uses) min_uses=cache[i].usecount;
+    }
+    for(size_t i=0; i<cache_size; ++i){
+        if(cache[i].inuse){
+            cache[i].usecount-=min_uses;
+            if(!cache[i].usecount) cache[i].inuse=false;
+        }
+    }
+}
+
+void cache_add(size_t deviceid, size_t sector, char *data){
+    size_t index=0;
+    if(!find_free(index)){
+        prune_cache();
+        if(!find_free(index)) return;
+    }
+    cache[index].deviceid=deviceid;
+    cache[index].sector=sector;
+    cache[index].usecount=0;
+    memcpy(cache[index].data, data, ATA_SECTOR_SIZE);
+    cache[index].inuse=true;
+}
+
+bool cache_get(size_t deviceid, size_t sector, char *data){
+    for(size_t i=0; i<cache_size; ++i){
+        if(cache[i].inuse && cache[i].deviceid==deviceid && cache[i].sector==sector){
+            cache[i].usecount++;
+            memcpy(data, cache[i].data, ATA_SECTOR_SIZE);
+            return true;
+        }
+    }
+    return false;
+}
+
+void cache_drop(size_t deviceid, size_t sector){
+    for(size_t i=0; i<cache_size; ++i) {
+        if (cache[i].inuse && cache[i].deviceid == deviceid && cache[i].sector == sector) {
+            cache[i].inuse=false;
+        }
+    }
+}
+
+}   // namespace ata
---
+++ mp3/student-distrib/drivers/ata/queue.cpp
@@ -0,0 +1,97 @@
+#include <operation_queue.hpp>
+#include "ata_priv.h"
+
+namespace ata {
+
+namespace ata_operation_types{
+    enum Enum{
+        Read,
+        Write,
+        Sync,
+        Shutdown,
+    };
+}
+
+namespace ata_operation_status{
+    enum Enum{
+        Pending,
+        Error,
+        Complete,
+    };
+}
+
+struct ata_operation{
+    ata_operation_types::Enum type;
+    ata_operation_status::Enum status;
+    ata_device *device;
+    uint32_t lba;
+    uint8_t *buf;
+    pid_t pid;
+};
+
+bool ata_queue_proc(ata_operation *op){
+    pid_t pid=getpid();
+    setpid(op->pid);
+    if(op->type==ata_operation_types::Read){
+        ata_device_read_sector(op->device, op->lba, op->buf);
+        op->status=ata_operation_status::Complete;
+    }else if(op->type==ata_operation_types::Write){
+        ata_device_write_sector_retry(op->device, op->lba, op->buf);
+        op->status=ata_operation_status::Complete;
+    }else if(op->type==ata_operation_types::Sync){
+        op->status=ata_operation_status::Complete;
+    }else{
+        return false;
+    }
+    setpid(pid);
+    return true;
+}
+
+typedef operation_queue<ata_operation, &ata_queue_proc, 128> ata_queue;
+ata_queue *queue;
+
+bool operation_blockcheck(void *p){
+    return ((ata_operation*)p)->status!=ata_operation_status::Pending;
+}
+
+void ata_sync(){
+    ata_operation op;
+    op.status=ata_operation_status::Pending;
+    op.type=ata_operation_types::Sync;
+    op.pid=getpid();
+    queue->add(&op);
+    thread_setblock(&operation_blockcheck, (void*)&op);
+}
+
+void init_queue(){
+    dbgout("ATA: Initialising queue...\n");
+    queue=new ata_queue();
+    dbgout("ATA: Syncing...\n");
+    ata_sync();
+}
+
+void ata_queued_read(ata_device *dev, uint32_t lba, uint8_t *buf){
+    ata_operation op;
+    op.status=ata_operation_status::Pending;
+    op.device=dev;
+    op.lba=lba;
+    op.buf=buf;
+    op.pid=getpid();
+    op.type=ata_operation_types::Read;
+    queue->add(&op);
+    thread_setblock(&operation_blockcheck, (void*)&op);
+}
+
+void ata_queued_write(ata_device *dev, uint32_t lba, uint8_t *buf){
+    ata_operation op;
+    op.status=ata_operation_status::Pending;
+    op.device=dev;
+    op.lba=lba;
+    op.buf=buf;
+    op.pid=getpid();
+    op.type=ata_operation_types::Write;
+    queue->add(&op);
+    thread_setblock(&operation_blockcheck, (void*)&op);
+}
+
+}   // namespace ata
