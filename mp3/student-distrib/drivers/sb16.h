79dd0c7ba22029929a0a26a46aa5de682a40b70d
Yu Wang <wangyu2165@gmail.com>
2015-12-06 15:57:19 -0600

    fixing compilation err

--- mp3/student-distrib/drivers/sb16.h
+++ mp3/student-distrib/drivers/sb16.h
@@ -1,11 +1,18 @@
-#ifndef _SB16_H
-#define _SB16_H
+#ifndef _SB16_H_
+#define _SB16_H_
 
 #include <stddef.h>
 #include <stdint.h>
 #include <inc/drivers/common.h>
 #include <inc/klibs/lib.h>
 #include <inc/i8259.h>
+#include <inc/fs/fops.h>
+#include <inc/fs/dev_wrapper.h>
+#include <inc/fs/filesystem.h>
+#include <inc/syscalls/filesystem_wrapper.h>
+
+using namespace filesystem;
+
 
 #define SB16_IRQ 5
 #define SB16_ID 5
@@ -20,9 +27,10 @@ void sb16_reset();
 void dma_start(uint8_t channel, uint32_t addr, uint32_t size, int8_t mode);
 void sb16_dsp_write(uint8_t byte);
 uint8_t sb16_dsp_read();
-void sb16_handler();
-int32_t play_wav(int8_t *filename);
-int32_t play_file(int32_t fd, int8_t bits, int8_t is_signed, uint16_t sample_rate);
+int sb16_handler(int irq, unsigned int saved_reg);
+// void sb16_handler();
+int32_t play_wav(char *filename);
+int32_t play_file(File& fd, int8_t bits, int8_t is_signed, uint16_t sample_rate);
 int32_t sb16_pause_playback();
 int32_t sb16_resume_playback();
 void sb16_stop_playback();
