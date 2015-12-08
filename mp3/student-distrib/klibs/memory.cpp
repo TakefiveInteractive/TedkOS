5880a85b462312457aaf4d5e8c41dd563307f40a
Yifei Teng <yifei@skycatch.com>
2015-12-08 15:39:13 -0600

    Sound plays

--- mp3/student-distrib/klibs/memory.cpp
+++ mp3/student-distrib/klibs/memory.cpp
@@ -1,4 +1,5 @@
 #include <inc/klibs/memory.h>
+#include <inc/klibs/palloc.h>
 
 namespace memory {
 
@@ -7,6 +8,11 @@ uint32_t ceil(uint32_t len, uint32_t blkSize)
     uint32_t remainder = len % blkSize;
     if (remainder > 0) return len / blkSize + 1;
     return len / blkSize;
+}
+
+uint32_t virtualAddressToPhysicalAddress(void* ptr)
+{
+    return (palloc::cpu0_memmap.translate(ptr).pde & ALIGN_4MB_ADDR) | (((uint32_t)ptr) & (~ALIGN_4MB_ADDR));
 }
 
 }
