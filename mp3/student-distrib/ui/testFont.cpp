5d30c30e265042e10cb2e9e1aa9f783ba1ea6732
Yifei Teng <yifei@skycatch.com>
2015-12-09 14:52:06 -0600

    Fix up fonts for once and for all\!

--- mp3/student-distrib/ui/testFont.cpp
+++ mp3/student-distrib/ui/testFont.cpp
@@ -12,22 +12,44 @@ using namespace filesystem;
 
 namespace ui {
 
-constexpr int numFont = 94;
-constexpr int FontWidth = 17;
-constexpr int FontHeight = 20;
+constexpr int NumFont = 94;
+constexpr int FontWidth = 20;
+constexpr int FontHeight = 42;
 constexpr int FontCharSpacing = 0;
 constexpr int FontLineSpacing = 8;
 
-TestFont::TestFont() : Drawable(FontWidth, FontHeight, 0, 0) {
+uint8_t *renderRGBAFont(ArrFile &parser, int width, int height)
+{
+    auto buffer = new uint8_t[CalcRGBASize(width * 40, height)];
+    for (int i = 0; i < 40; i++)
+    {
+        uint8_t *grayScaleFont = (uint8_t*) parser[i + 65];
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                uint8_t *pix = buffer + (y * width * 40 + x + i * width) * 4;
+                uint8_t *grayPix = grayScaleFont + (y * width + x);
+                pix[0] = 0;
+                pix[1] = 0;
+                pix[2] = 0;
+                pix[3] = grayPix[0];
+            }
+        }
+    }
+    return buffer;
+}
+
+TestFont::TestFont() : Drawable(FontWidth * 40, FontHeight, 0, 0) {
     File fontFile;
     struct stat st;
-    theDispatcher->open(fontFile, "inconsolata_15.carr");
+    theDispatcher->open(fontFile, "inconsolata_36.carr");
     theDispatcher->fstat(fontFile, &st);
     const uint32_t size = st.st_size;
     auto buffer = new uint8_t[size];
     theDispatcher->read(fontFile, buffer, size);
     ArrFile* fileParser = ArrFile::getInstance((char*) buffer);
-    pixelBuffer = (uint8_t*) (*fileParser)[70];
+    pixelBuffer = renderRGBAFont(*fileParser, FontWidth, FontHeight);
     theDispatcher->close(fontFile);
 }
 
