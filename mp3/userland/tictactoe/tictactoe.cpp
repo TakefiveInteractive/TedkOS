229a510aefbc0be5c33a39217684ade118601a05
Yu Wang <wangyu2165@gmail.com>
2015-12-09 23:54:39 -0600

    load images and buttons

--- mp3/userland/tictactoe/tictactoe.cpp
+++ mp3/userland/tictactoe/tictactoe.cpp
@@ -4,6 +4,7 @@
 
 tictactoe::tictactoe() {
     drawBoardWindow();
+    loadImageResources();
 }
 
 auto tictactoe::drawBoardWindow() -> void {
@@ -14,14 +15,37 @@ auto tictactoe::drawBoardWindow() -> void {
 
     // Draw background image on main window
     void *background = TedCreateImage(WindowSize, WindowSize, 0, 0);
-    FILE *fp = fopen ("tictactoe_bg", "rb");
+
+    TedSetImageData(background, backgroundImageBuffer);
+    TedElemShow(background);
+}
+
+auto loadImageHelper(char *ptr, char *filename) -> void {
+    FILE *fp = fopen (filename, "rb");
     fseek(fp, 0L, SEEK_END);
     auto sz = ftell(fp);
     fseek(fp, 0L, SEEK_SET);
-    char *backgroundImageBuffer = new char[sz];
-    fread(backgroundImageBuffer, sz, 1, fp);
-    TedSetImageData(background, backgroundImageBuffer);
-    TedElemShow(background);
+    ptr = new char[sz];
+    fread(ptr, sz, 1, fp);
+    fclose(fp);
+}
+
+auto tictactoe::loadImageResources() -> void {
+    loadImageHelper(backgroundImageBuffer, (char *)"tictactoe_bg_img");
+    loadImageHelper(ODropImageBuffer, (char *)"tictactoe_o_img");
+    loadImageHelper(XDropImageBuffer, (char *)"tictactoe_x_img");
+}
+
+auto tictactoe::drawButtons() -> void {
+    constexpr int SingleSize = WindowSize / 3;
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            void *dropButton = TedCreateImage(SingleSize, SingleSize, SingleSize * j, SingleSize * i);
+            TedAttachMessageHandler(dropButton, (void *)(i * 3 + j));
+            TedElemShow(dropButton);
+        }
+    }
 }
 
 auto tictactoe::checkWin(int row, int col) -> bool {
@@ -36,7 +60,10 @@ auto tictactoe::checkWin(int row, int col) -> bool {
 auto tictactoe::runGame() -> void {
     drawBoardWindow();
     for (int i = 0; i < 9; i++) {
-
+        void *message;
+        TedGetMessage(message);
+        int button_tag = (int)(message);
+        if (isOTerm)
         isOTerm = !isOTerm;
     }
     gameEndWithDraw();
